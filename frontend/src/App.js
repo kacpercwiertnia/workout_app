6e7ef69d4d6a4844880b4a47309a94d8e55a4a1c
kacpercwiertnia <kacpercwiertnia02@gmail.com>
2023-04-11 22:22:40 +0200

    basic fetch test

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import MusclesListPage from './pages/MusclesListPage';
+
+function App() {
+  return (
+    <div className="App">
+      <MusclesListPage/>
+    </div>
+  );
+}
+
+export default App;
